df54617a39b4b1e5aaee51cf03a9283b212bdf6f
Douglas Moura <douglas.ademoura@gmail.com>
2022-01-09 17:35:06 -0300

    chore: change snake_case to camelCase

--- src/formatMap.js
+++ src/formatMap.js
@@ -2,10 +2,9 @@ module.exports = {
   root: content => `${content !== undefined ? `root = ${content ? 'true' : 'false'}\n\n` : ''}`,
   glob: content => `[${content}]\n`,
   charset: content => `charset = ${content}\n`,
-  end_of_line: content => `end_of_line = ${content}\n`,
-  insert_final_newline: content => `insert_final_newline = ${content}\n`,
-  indent_style: content => `indent_style = ${content}\n`,
-  indent_size: content => `indent_size = ${content}\n`,
-  trim_trailing_whitespace: content => `trim_trailing_whitespace = ${content}\n`,
-  final: _content => ''
-};
+  endOfLine: content => `end_of_line = ${content}\n`,
+  insertFinalNewline: content => `insert_final_newline = ${content}\n`,
+  indentStyle: content => `indent_style = ${content}\n`,
+  indentSize: content => `indent_size = ${content}\n`,
+  trimTrailingWhitespace: content => `trim_trailing_whitespace = ${content}\n`,
+}
